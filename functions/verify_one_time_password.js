2bb3020682ed9cd31ac6bafb8e1613285fb92b6d
Inseong Park <rarira@gmail.com>
2018-02-23 01:38:10 +0900

    refactoring finished section 7

--- functions/verify_one_time_password.js
+++ functions/verify_one_time_password.js
@@ -20,12 +20,10 @@ module.exports = function(req, res) {
           return res.status(422).send({ error: 'Code not valid'});
         }
 
+        ref.update({ codeValid: false })
+        .then(() =>admin.auth().createCustomToken(phone))
+        .then(token => res.send({ token: token}))
       });
     })
-    .then(() => ref.update({ codeValid: false }))
-    .then(() =>admin.auth().createCustomToken(phone))
-    .then(token => res.send({ token: token}))
-
-
     .catch(err => res.status(422).send({ error: err }));
 }
